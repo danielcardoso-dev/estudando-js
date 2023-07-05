6ec35f23149e8543f34c4812c399813571a15824
danielcardoso-dev <danielcardoso.10.12.dc@gmail.com>
2023-07-05 09:51:42 -0300

    concatenacao

---
+++ aula-050723/variaveis-concatenacao.js
@@ -0,0 +1,23 @@
+// nunca repetir a mesma variavel
+// nunca começar a variavel com o primeiro caracter. Pode começar com "_","$" (exceções).
+//let numeros = 14;
+//let numeroe = 14;
+//let n23umero = 90;
+//let _ = 67;
+//let $ = 56;
+////////////////////////////////////////////////////////////////
+
+// Operador de Concatenação +
+const numero = 2 + 3; // Operador de Adição
+const texto = "O numero é " + numero; // Operador de concatenação
+// console.log(texto);
+
+// console.log("A soma de " + 2 + " com " + 3 + " é igual a " + numero);
+
+let idade = 8;
+let mensagem = (idade >= 18) ? "Sua idade é " + idade + " pode ser preso!" : "Sua idade é " + idade + " vai chegar tua vez. Te cuida!";
+// console.log(mensagem);
+
+const nome = "Gabriel";
+console.log("Bem-vindo, " + nome);
+
