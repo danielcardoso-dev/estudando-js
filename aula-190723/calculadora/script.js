e34302f3d645bb463b687eccf4163ee81806acd1
danielcardoso-dev <danielcardoso.10.12.dc@gmail.com>
2023-07-19 16:28:04 -0300

    calculadora

---
+++ aula-190723/calculadora/script.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll(".pressable");
+const btnAC = document.querySelector("#ac");
+const btnCalc = document.querySelector("#calc");
+const visor = document.querySelector("#visor");
+
+for(let btn of btns) {
+    btn.addEventListener("click", (evento) => {
+        const num = evento.target.innerText;
+        visor.value += num;
+    });
+}
+
+btnAC.addEventListener("click", () => {
+    visor.value = "";
+});
+
+btnCalc.addEventListener("click", () => {
+    let expressao = visor.value.replaceAll(",",".");
+    let resultado =  eval(expressao).toString().replaceAll(",",".");
+    visor.value = resultado;
+})
